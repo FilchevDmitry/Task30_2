d979f890031ee12388034113a571b3813b5d1d26
Dmitry <filchev_dmitry@mail.ru>
2023-02-25 20:48:22 +0300

    Добавьте файлы проекта.

---
+++ Task30_2/Task30_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include<cpr/cpr.h>
+
+int main()
+{
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"),
+		cpr::Header({ {"accept:","text/html"} }));
+	std::string out;
+	if (r.text.find("<h1>") != r.text.size())
+	{
+		for (int i = r.text.find("<h1>") + 4; i < r.text.find("</h1>"); i++)
+		{
+			out += r.text[i];
+		}
+		std::cout << out << std::endl;
+	}
+	std::cout << "status code " << r.status_code << std::endl;
+}
